a9f05695b0c6728ba0c4343965ec149526e451d8
Roburlion <94622793+Roburlion@users.noreply.github.com>
2022-01-14 02:51:05 -0600

    number game working

---
+++ numbers.js
@@ -0,0 +1,60 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let guessCount = 1;
+
+document.querySelector('#submit-guess').addEventListener('click', () => {
+    // the get guess part
+    const userGuess = Number(document.getElementById('number-guess').value)
+
+    // check guess
+    const guessResult = (userGuess, randomNumber) => {
+        if (userGuess < randomNumber) return 'is too low'
+        if (userGuess > randomNumber) return 'is too high'
+        return 'is CORRECT!'
+    }
+
+    const result = guessResult(userGuess, randomNumber);
+    
+    // the record guess part
+    const newGuessElement = document.createElement('li');
+    newGuessElement.textContent = `${userGuess} ${result}`
+    document.getElementById('random-number-output').appendChild(newGuessElement)
+    
+    // reset for next guess
+    document.getElementById('number-guess').value = ''
+    document.getElementById('number-guess').focus()
+    guessCount++
+
+    if (userGuess === randomNumber || guessCount > 7) {
+        // disable input & submit button
+        document.getElementById('number-guess').disabled = true;
+        document.getElementById('submit-guess').disabled = true;
+        // if win, set background to green and print !!!You've won!!!
+        if (userGuess === randomNumber) {
+            document.getElementById('number-game-container').style.backgroundColor = 'green';
+            
+            const element = document.createElement('li');
+            element.textContent = `!!!You've WON!!!`
+            element.style.fontSize = '4rem'
+            element.style.listStyle = 'none'
+            document.getElementById('random-number-output').appendChild(element)
+        }
+        // if lose, set background to red and print !!!You've lost!!!
+        if (guessCount > 7) {
+            document.getElementById('number-game-container').style.backgroundColor = 'red';
+            
+            const element = document.createElement('li');
+            element.textContent = `!!!You've LOST!!!`
+            element.style.fontSize = '4rem'
+            element.style.listStyle = 'none'
+            document.getElementById('random-number-output').appendChild(element)
+        }
+        // add "play another round" button that resets the game
+        const resetButton = document.createElement('button');
+        resetButton.textContent = 'Play another game'
+        resetButton.id = 'resetButton'
+        document.getElementById('number-game-container').appendChild(resetButton)
+        document.getElementById('resetButton').addEventListener('click', () => {
+            location.reload()
+        })
+    }
+})
